2a76b01b9237a9972bfde8292dc4ec14793f5bb2
zainabzahid711 <zainabzahid711@gmail.com>
2024-02-15 14:33:59 +0500

    modifications

--- basic-portfolio/src/components/Portfolio/Portfolio.js
+++ basic-portfolio/src/components/Portfolio/Portfolio.js
@@ -2,12 +2,16 @@ import React from "react";
 import style from "./Portfolio.module.css";
 import Sidebar from "../SideBar/Sidebar";
 import Header from "../header/Header";
+import BgImage from "../bgImage/BgImage";
+import Description from "../Description/Description";
 
 function Portfolio() {
   return (
     <div className={style.Portfolio}>
       <Header />
+      <BgImage />
       <Sidebar />
+      <Description />
     </div>
   );
 }
