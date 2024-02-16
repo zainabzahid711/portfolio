7ab680dce8a5d7baf51975165f4331262751d7c0
zainabzahid711 <zainabzahid711@gmail.com>
2024-02-16 23:28:36 +0500

    services added

---
+++ basic-portfolio/src/components/Services/Services.js
@@ -0,0 +1,47 @@
+import React from "react";
+import style from "./Services.module.css";
+
+function Services() {
+  return (
+    <div className={style.mainServices}>
+      <div className={style.mainTextservice}>
+        <h3 className={style.ServiceHeading}> Services </h3>
+      </div>
+      <div className={style.subServ2}>
+        <div className={style.subServic}>
+          <h5 className={style.subserHeading}> UI/UX Design </h5>
+          <p className={style.servicePara}>
+            This includes creating wireframes, mockups, and prototypes for
+            websites and web applications. Also offer UI/UX audits to help.
+          </p>
+        </div>
+        <div className={style.subServic}>
+          <h5 className={style.subserHeading}> Web Development </h5>
+          <p className={style.servicePara}>
+            This includes building web applications using React for the frontend
+            and Node.js. Also offer services such as authentication and
+            deployment.
+          </p>
+        </div>
+      </div>
+      <div className={style.subServ2}>
+        <div className={style.subServic}>
+          <h5 className={style.subserHeading}> Performance Optimization </h5>
+          <p className={style.servicePara}>
+            This includes optimizing the performance of websites and web
+            applications to improve speed, responsiveness, and user experience.
+          </p>
+        </div>
+        <div className={style.subServic}>
+          <h5 className={style.subserHeading}> Serverless Architecture </h5>
+          <p className={style.servicePara}>
+            This includes building serverless applications using platforms such
+            as AWS Lambda, Azure Functions, and Google Cloud Functions.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
